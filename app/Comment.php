16021ad890a8ded3f76ed7154502e3a6043555ab
RehamMobarak <reham.a.mobarak@gmail.com>
2020-01-08 04:18:16 +0200

    added comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable=['body','commentable_id'];
+    protected $fillable=['body'];
     
     public function user()
     {
